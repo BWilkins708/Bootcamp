824b1a66e7937f0db3beef9325262f3aff701889
BWilkins708 <87435601+BWilkins708@users.noreply.github.com>
2022-01-05 15:24:29 +0000

    Add files via upload

---
+++ crewMember.js
@@ -0,0 +1,15 @@
+class crewMember {
+
+    //Properties
+    name;
+    position;
+    staffNumber;
+
+    constructor(name, position, staffNumber) {
+        this.name = name
+        this.position = position
+        this.staffNumber = staffNumber
+    }
+}
+
+const vicky = new crewMember('vicky', 'Air hostess', '337');
