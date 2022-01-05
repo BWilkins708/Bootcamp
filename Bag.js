5c4e35509ccce0fd7544fd70958f19b62134ec55
BWilkins708 <87435601+BWilkins708@users.noreply.github.com>
2022-01-05 15:23:21 +0000

    Add files via upload

---
+++ Bag.js
@@ -0,0 +1,29 @@
+class Bag {
+
+    // properties
+    weight;
+    
+    /**
+     * Constructs a new instance of the Bag class
+     * @param {Number} weight 
+     */
+    constructor(someWeight) {
+        // set the weight that is passed in to be the weight of "this" new instance we are constructing
+        this.weight = someWeight;
+    }
+
+    isOverLimit() {
+        // TODO - add code to check if weight of the bag is over the limit (23kg)
+        if(this.weight <= 23) {
+            console.log("This bag is correct weight");
+        }else 
+            console.log("This bag is over limit");
+    }
+}
+
+// Test code
+const mandyBag = new Bag(16);
+const ucheBag = new Bag(25);
+
+console.log(mandyBag.isOverLimit()); 
+console.log(ucheBag.isOverLimit()); 
